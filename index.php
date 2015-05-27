bf51edb02b4002bf84640efbd18eccd0eccb54d4
Jonathan Lam <jlam55555@gmail.com>
2015-05-26 22:39:01 -0400

    Changed hr width. Added hr to homepage.

--- index.php
+++ index.php
@@ -6,5 +6,6 @@
 	?>
 	<H2>Diagrams</H2>
 	<H6>If you have not filled out the survey (and are from Joel Barlow High School, please fill out the <A href="survey.php">survey</A> so that data can be obtained.)</H6>
+	<HR />
 	<H2 style="color: red;">This page is still a work in progress. Hopefully it will be done by Wednesday or Thursday. For now, please complete the survey.</H2>
 <?php include "part/bottom.txt" ?>
